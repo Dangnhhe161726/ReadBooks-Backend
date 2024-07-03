57e4a51d317ccf76d4d803bee33e6d39f708636b
Chu-duong <demonss0910@gmail.com>
2024-07-03 19:25:19 +0700

    add category controller

--- backend/src/main/java/com/example/backend/services/category/ICategoryService.java
+++ backend/src/main/java/com/example/backend/services/category/ICategoryService.java
@@ -1,6 +1,18 @@
 package com.example.backend.services.category;
 
+import com.example.backend.models.entities.Category;
+import com.example.backend.models.responses.CategoryResponse;
+import java.util.List;
+
 public interface ICategoryService {
 
     void createCategoies(String name);
+
+  CategoryResponse getById(Long id);
+
+  List<CategoryResponse> getAll();
+
+  CategoryResponse save(CategoryResponse product);
+
+  void delete(Long id);
 }
