2a30a6d01d3fcb820f95d0a4111cc8d8ff108a0f
Chu-duong <demonss0910@gmail.com>
2024-07-04 19:31:33 +0700

    add getBooksByUserId controller

--- backend/src/main/java/com/example/backend/controllers/book/BookController.java
+++ backend/src/main/java/com/example/backend/controllers/book/BookController.java
@@ -1,54 +1,77 @@
 package com.example.backend.controllers.book;
 
 import com.example.backend.models.dtos.BookDTO;
+import com.example.backend.models.responses.AuthorResponse;
 import com.example.backend.models.responses.BookResponse;
 import com.example.backend.models.responses.HttpResponse;
 import com.example.backend.services.amazons3.IAmazonS3Service;
 import com.example.backend.services.book.IBookService;
 import com.example.backend.services.cloudinary.ICloudinaryService;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${api.prefix}/book")
 @RequiredArgsConstructor
 public class BookController {
-    private final IBookService bookService;
-    private final ICloudinaryService cloudinaryService;
-    private final IAmazonS3Service amazonS3Service;
-    private String timeStamp = LocalDateTime.now().toString();
 
-    @PostMapping("/create")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<HttpResponse> createBook(
-            @RequestBody BookDTO bookDTO) {
-        try {
-            BookResponse newBook = bookService.createBook(bookDTO);
-            return ResponseEntity.ok().body(
-                    HttpResponse.builder()
-                            .timeStamp(timeStamp)
-                            .status(HttpStatus.OK)
-                            .statusCode(HttpStatus.OK.value())
-                            .message("Login successfull")
-                            .data(Map.of("Book", newBook))
-                            .build()
-            );
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(
-                    HttpResponse.builder()
-                            .statusCode(HttpStatus.BAD_REQUEST.value())
-                            .timeStamp(timeStamp)
-                            .message(e.getMessage())
-                            .build()
-            );
-        }
+  private final IBookService bookService;
+  private final ICloudinaryService cloudinaryService;
+  private final IAmazonS3Service amazonS3Service;
+  private String timeStamp = LocalDateTime.now().toString();
+
+  @PostMapping("/create")
+  @PreAuthorize("hasAnyAuthority('ADMIN')")
+  public ResponseEntity<HttpResponse> createBook(
+      @RequestBody BookDTO bookDTO) {
+    try {
+      BookResponse newBook = bookService.createBook(bookDTO);
+      return ResponseEntity.ok().body(
+          HttpResponse.builder()
+              .timeStamp(timeStamp)
+              .status(HttpStatus.OK)
+              .statusCode(HttpStatus.OK.value())
+              .message("Login successfull")
+              .data(Map.of("Book", newBook))
+              .build()
+      );
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(
+          HttpResponse.builder()
+              .statusCode(HttpStatus.BAD_REQUEST.value())
+              .timeStamp(timeStamp)
+              .message(e.getMessage())
+              .build()
+      );
     }
+  }
 
+  @GetMapping("/user/{id}")
+  public ResponseEntity<HttpResponse> getBooksByUserId(
+      @PathVariable("id") @NotNull(message = "error.request.path.variable.id.invalid") Long id) {
+    List<BookResponse> books = bookService.getBooksByUserId(id);
+
+    return ResponseEntity.ok().body(
+        HttpResponse.builder()
+            .timeStamp(timeStamp)
+            .status(HttpStatus.OK)
+            .statusCode(HttpStatus.OK.value())
+            .message("Login successfull")
+            .data(Map.of("Books", books))
+            .build()
+    );
+  }
 
 }
--- backend/src/main/java/com/example/backend/services/book/IBookService.java
+++ backend/src/main/java/com/example/backend/services/book/IBookService.java
@@ -2,21 +2,24 @@ package com.example.backend.services.book;
 
 import com.example.backend.models.dtos.BookDTO;
 import com.example.backend.models.responses.BookResponse;
+import java.util.List;
 import lombok.NonNull;
 import org.springframework.data.domain.Page;
 
 public interface IBookService {
 
-    Page<BookDTO> getByPaging(
-            int pageNo, int pageSize, String sortBy, String sortDirection, String keyword);
+  Page<BookDTO> getByPaging(
+      int pageNo, int pageSize, String sortBy, String sortDirection, String keyword);
 
-    BookDTO getById(Long id);
+  BookDTO getById(Long id);
 
-    BookDTO save(BookDTO category);
+  BookDTO save(BookDTO category);
 
-    BookDTO update(@NonNull BookDTO category);
+  BookDTO update(@NonNull BookDTO category);
 
-    BookResponse createBook(@NonNull BookDTO bookDTO) throws Exception;
+  BookResponse createBook(@NonNull BookDTO bookDTO) throws Exception;
 
-    BookResponse createLinkAndThumnail(Long id, String thumnail, String Link) throws Exception;
+  BookResponse createLinkAndThumnail(Long id, String thumnail, String Link) throws Exception;
+
+  List<BookResponse> getBooksByUserId(@NonNull Long id);
 }
